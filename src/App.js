f74cd49bd4cedf5235e0e23857017df26b433e7b
geremews <samuel.geremew@specinnovations.com>
2023-06-13 23:13:14 -0400

    basic layout and started styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+  const surpriseOptions = [
+    'A blue ostrich eating melon',
+    'A matisse style shark on the telephone',
+    'A pineapple sunbathing on an island'
+  ];
+
   return (
     <div className="app">
       <section className="search-section">
-        <p>Start with a detailed description <span className="surprise">Surprise me</span></p>
+        <p>Start with a detailed description
+          <span className="surprise">Surprise me</span>
+        </p>
+        <div className="input-container">
+          <input placeholder="An impressionist oil painting of a sunflower in a purple vase..."/>
+          <button>Generate</button>
+        </div>
       </section>
       <section className="image-section"></section>
     </div>
